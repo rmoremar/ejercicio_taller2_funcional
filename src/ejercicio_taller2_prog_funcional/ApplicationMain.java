d658fb029240d748250094900392543a2d38efbe
rmoremar <rmoremar@emeal.nttdata.com>
2022-04-08 13:03:22 +0200

    first final

--- src/ejercicio_taller2_prog_funcional/ApplicationMain.java
+++ src/ejercicio_taller2_prog_funcional/ApplicationMain.java
@@ -3,7 +3,7 @@ package ejercicio_taller2_prog_funcional;
 import java.math.BigDecimal;
 import java.util.List;
 
-public class asdas {
+public class ApplicationMain {
 	public static void main(String[] args) {
 
 		List<Product> shoppingCart = List.of(new Product("Clothes", new BigDecimal("15.90"), Tax.NORMAL),
@@ -14,7 +14,8 @@ public class asdas {
 				new Product("Whiskey", new BigDecimal("19.90"), Tax.NORMAL));
 
 		// Implementación
-
+		
+		//Mostrar total
 		BigDecimal total = new BigDecimal("0.0");
 		for (Product product : shoppingCart) {
 			total = total.add(product.applyTax());
@@ -22,6 +23,7 @@ public class asdas {
 
 		System.out.println("Precio total con IVA: " + total);
 
+		//Mostrar productos que empiezan por C
 		System.out.println("Productos que empizan por la letra 'C'");
 		shoppingCart.forEach(prod -> {
 			if (prod.getName().charAt(0) == 'C') {
